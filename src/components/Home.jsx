62737e989d63d166b482887ef29bff48f8f493f8
Hpro78891 <handsomeprogrammer753@gmail.com>
2022-11-24 19:21:19 +0530

    set image height and width for responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,55 +1,70 @@
-import React from 'react'
-import {background, Box, Heading,Image,Container,Stack,Text} from '@chakra-ui/react';
-import { Carousel } from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import img1 from "../assets/1.jpg";
+import React from 'react';
+import {
+  background,
+  Box,
+  Heading,
+  Image,
+  Container,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
-import img3 from "../assets/3.jpg";
+import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
 
 const headingOptions = {
-    pos: 'absolute',
-    left:'50%',
-    top:'50%',
-    transform:'translate(-50%,-50%)',
-    textTransform:'uppercase',
-    p:'4',
-    size:'4xl'
-
-}
-
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
 
 const Home = () => {
   return (
- <Box>
-    <MyCarousel />
-    <Container maxW={'container.xl'} minH={'100vh'} p='16'>
+    <Box>
+      <MyCarousel />
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
         <Heading
-            textTransform={'uppercase'}
-            py="2"
-            w={'fit-content'}
-            borderBottom={'2px solid'}
-            m="auto">
-                Services
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
         </Heading>
-        <Stack h="full" p={'4'} alignItems={'center'} direction={['column','row']}>
-         
-            <Image src={img5} h={['40','400']} filter={'hue-rotate(-130deg)'} />
-        <Text letterSpacing={'widset'} lineHeight={'190%'} p={["4","16"]} textAlign={"center"}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Fuga amet similique quia! Quis quidem dignissimos saepe
-             excepturi illum exercitationem aliquid incidunt deleniti impedit!
-              Accusamus ratione commodi qui debitis quo, quaerat itaque et.
-               Excepturi maxime sapiente, dolore sed ipsa expedita exercitationem
-                numquam? Animi ex incidunt recusandae tempora sed, quo exercitationem explicabo!
-
-        </Text>
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+          <Text
+            letterSpacing={'widset'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga amet
+            similique quia! Quis quidem dignissimos saepe excepturi illum
+            exercitationem aliquid incidunt deleniti impedit! Accusamus ratione
+            commodi qui debitis quo, quaerat itaque et. Excepturi maxime
+            sapiente, dolore sed ipsa expedita exercitationem numquam? Animi ex
+            incidunt recusandae tempora sed, quo exercitationem explicabo!
+          </Text>
         </Stack>
-    </Container>
- </Box>
-  )
-}
+      </Container>
+    </Box>
+  );
+};
 
 const MyCarousel = () => (
   <Carousel
@@ -61,31 +76,30 @@ const MyCarousel = () => (
     showArrows={false}
   >
     <Box w="full" h={'100vh'}>
-      <Image src={img1} />
+      <Image src={img1} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.100'} color={'black'} {...headingOptions}>
         Watch The Future
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img2} />
+      <Image src={img2} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'WhiteAplha.200'} color={'white'} {...headingOptions}>
         Future is Gaming
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img3} />
+      <Image src={img3} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAplha.600'} color={'white'} {...headingOptions}>
         Gaming on console
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img4} />
+      <Image src={img4} h={'full'} w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAplha.600'} color={'white'} {...headingOptions}>
         Let's go buddy
       </Heading>
     </Box>
-  
   </Carousel>
 );
 
-export default Home
+export default Home;
